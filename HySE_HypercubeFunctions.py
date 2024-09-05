a4d6962d1bdab6234eb836117b26de362169989e
Isabelle Racicot <racicot.isabelle@gmail.com>
2024-09-05 14:56:22 +0100

    Debugging

--- HySE_HypercubeFunctions.py
+++ HySE_HypercubeFunctions.py
@@ -1183,6 +1183,8 @@ def Rescale(im, PercMax, Crop=True):
 ####################################################################
 
 import SimpleITK as sitk
+import time
+from tqdm.notebook import trange, tqdm, tnrange
 
 
 def SweepCoRegister(DataSweep, **kwargs):
@@ -1206,7 +1208,7 @@ def SweepCoRegister(DataSweep, **kwargs):
 	Outputs:
 
 	"""
-	AllIndices = [DataSweep[i][0] for i in range(0,len(DataSweep))]
+	AllIndices = [DataSweep[i].shape[0] for i in range(0,len(DataSweep))]
 	MaxIndex = np.amax(AllIndices)
 
 	try:
@@ -1243,24 +1245,28 @@ def SweepCoRegister(DataSweep, **kwargs):
 		except KeyError:
 			SavingPath = ''
 			print(f'PlotDiff has been set to True. Indicate a SavingPath.')
-		
-		try: 
-			Plot_PlateauList = kwargs['Plot_PlateauList']
-		except:
-			Plot_PlateauList = 'All'
+	try: 
+		Plot_PlateauList = kwargs['Plot_PlateauList']
+	except:
+		Plot_PlateauList = 'All'
+	
 
-		try: 
-			Plot_Index = kwargs['Plot_Index']
-		except:
-			Plot_Index = 14
-			if MaxIndex>Plot_Index:
-				Plot_Index = int(MaxIndex/2)
-				print(f'Plot_Index outside default range. Set to {Plot_Index}, please set manually with Plot_Index')
+	try: 
+		Plot_Index = kwargs['Plot_Index']
+	except:
+		Plot_Index = 14
+		if MaxIndex>Plot_Index:
+			Plot_Index = int(MaxIndex/2)
+			print(f'Plot_Index outside default range. Set to {Plot_Index}, please set manually with Plot_Index')
 
 
 	print(f'Static image: plateau {ImStatic_Plateau}, index {ImStatic_Index}. Use ImStatic_Plateau and ImStatic_Index to change it.')
 	print(f'Buffer set to {Buffer}')
 
+	if Plot_PlateauList=='All' or Plot_PlateauList=='None':
+		PlotPlateauString = True
+	else:
+		PlotPlateauString = False
 
 	t0 = time.time()
 	Ncolours = len(DataSweep)
@@ -1272,28 +1278,35 @@ def SweepCoRegister(DataSweep, **kwargs):
 	im_static = DataSweep[ImStatic_Plateau][ImStatic_Index,:,:]
 
 	## Loop through all colours (wavelengths)
-	for c in range(0, Ncolours):
-	    if c==8: ## ignore dark
-	        # print(f'DARK')
-	        pass
-	    else:
-	        ImagesTemp = []
-	        (NN, YY, XX) = DataSweep[c].shape
-	        for i in range(buffer,NN-buffer):
-	            im_shifted = DataSweep[c][i,:,:]
-	            im_coregistered, shift_val, time_taken = CoRegisterImages(im_static, im_shifted)
-	            ImagesTemp.append(im_coregistered)
+	for c in tnrange(0, Ncolours):
+		if c==8: ## ignore dark
+			# print(f'DARK')
+			pass
+		else:
+			ImagesTemp = []
+			(NN, YY, XX) = DataSweep[c].shape
+			for i in range(Buffer,NN-Buffer):
+				im_shifted = DataSweep[c][i,:,:]
+				im_coregistered, shift_val, time_taken = CoRegisterImages(im_static, im_shifted)
+				ImagesTemp.append(im_coregistered)
 
-	            ## Plot co-registration is requested
-	            if c in Plot_PlateauList or Plot_PlateauList=='All':
-	            	if i==Plot_Index:
-	            		Name = f'Plateau{c}_Index{i}_CoRegistration.png'
-	            		PlotCoRegistered(im_static, im_shifted, im_coregistered, SavePlot=True, SavingPathWithName=SavingPath+Name)
-	        
-	        ImagesTemp = np.array(ImagesTemp)
-	        ImAvg = np.average(ImagesTemp, axis=0)
-	        Hypercube.append(ImAvg)
-	        
+				## Plot co-registration is requested
+				if PlotDiff:
+					if PlotPlateauString:
+						if Plot_PlateauList=='All':
+							if i==Plot_Index:
+								Name = f'Plateau{c}_Index{i}_CoRegistration.png'
+								PlotCoRegistered(im_static, im_shifted, im_coregistered, SavePlot=True, SavingPathWithName=SavingPath+Name)
+					else:
+						if c in Plot_PlateauList:
+							if i==Plot_Index:
+								Name = f'Plateau{c}_Index{i}_CoRegistration.png'
+								PlotCoRegistered(im_static, im_shifted, im_coregistered, SavePlot=True, SavingPathWithName=SavingPath+Name)
+			
+			ImagesTemp = np.array(ImagesTemp)
+			ImAvg = np.average(ImagesTemp, axis=0)
+			Hypercube.append(ImAvg)
+			
 	tf = time.time()
 	Hypercube = np.array(Hypercube)
 	## Calculate time taken
@@ -1307,36 +1320,36 @@ def SweepCoRegister(DataSweep, **kwargs):
 
 
 def FindPlottingRange(array):
-    array_flat = array.flatten()
-    array_sorted = np.sort(array_flat)    
-    mean = np.average(array_sorted)
-    std = np.std(array_sorted)
-    MM = mean+3*std
-    mm = mean-3*std
-    return mm, MM
+	array_flat = array.flatten()
+	array_sorted = np.sort(array_flat)    
+	mean = np.average(array_sorted)
+	std = np.std(array_sorted)
+	MM = mean+3*std
+	mm = mean-3*std
+	return mm, MM
 
 def CoRegisterImages(im_static, im_shifted):
-    t0 = time.time()
-    ## Convert the numpy array to simple elestix format
-    im_static_se = sitk.GetImageFromArray(im_static)
-    im_shifted_se = sitk.GetImageFromArray(im_shifted)
+	t0 = time.time()
+	## Convert the numpy array to simple elestix format
+	im_static_se = sitk.GetImageFromArray(im_static)
+	im_shifted_se = sitk.GetImageFromArray(im_shifted)
 
-    ## Create object
-    elastixImageFilter = sitk.ElastixImageFilter()
-    
-    ## Turn off console
-    elastixImageFilter.LogToConsoleOff()
-    
-    ## Set image parameters
-    elastixImageFilter.SetFixedImage(im_static_se)
-    elastixImageFilter.SetMovingImage(im_shifted_se)
-    
-    ## Set transform parameters
-    parameterMap = sitk.GetDefaultParameterMap('translation')
-    parameterMap['Transform'] = ['BSplineTransform']
-    
-    ## Parameters to play with if co-registration is not optimal:
-    
+	## Create object
+	elastixImageFilter = sitk.ElastixImageFilter()
+	
+	## Turn off console
+	elastixImageFilter.LogToConsoleOff()
+	
+	## Set image parameters
+	elastixImageFilter.SetFixedImage(im_static_se)
+	elastixImageFilter.SetMovingImage(im_shifted_se)
+	
+	## Set transform parameters
+	parameterMap = sitk.GetDefaultParameterMap('translation')
+	parameterMap['Transform'] = ['BSplineTransform']
+	
+	## Parameters to play with if co-registration is not optimal:
+	
 #         # Controls how long the optimizer runs
 #     parameterMap['MaximumNumberOfIterations'] = ['500'] 
 #         # You can try different metrics like AdvancedMattesMutualInformation, NormalizedCorrelation, 
@@ -1359,7 +1372,7 @@ def CoRegisterImages(im_static, im_shifted):
 #     parameterMap['AutomaticTransformInitialization'] = ['true']
 #         # Controls the interpolation order for the final transformation.
 #     parameterMap['FinalBSplineInterpolationOrder'] = ['3']
-    
+	
 # #         # Adjust the maximum step length for the optimizer
 # #     parameterMap['MaximumStepLength'] = ['4.0']
 # #         # Use more samples for computing gradients
@@ -1368,140 +1381,140 @@ def CoRegisterImages(im_static, im_shifted):
 # #     parameterMap['FinalGridSpacingInVoxels'] = ['8.0']
 # #         # Defines the spacing of the sampling grid used during optimization.
 # #     parameterMap['SampleGridSpacing'] = ['2.0']
-        
-    
-    ## If required, set maximum number of iterations
+		
+	
+	## If required, set maximum number of iterations
 #     parameterMap['MaximumNumberOfIterations'] = ['500']
-    elastixImageFilter.SetParameterMap(parameterMap)
-    
-    ## Execute
-    result = elastixImageFilter.Execute()
-    ## Convert result to numpy array
-    im_coregistered = sitk.GetArrayFromImage(result)
-    t1 = time.time()
-    
-    ## Find time taken:
-    time_taken = t1-t0
-    
-    ## Get an idea of difference
-    shift_val = np.average(np.abs(np.subtract(im_static,im_coregistered)))
-    
-    ## return 
-    return im_coregistered, shift_val, time_taken
+	elastixImageFilter.SetParameterMap(parameterMap)
+	
+	## Execute
+	result = elastixImageFilter.Execute()
+	## Convert result to numpy array
+	im_coregistered = sitk.GetArrayFromImage(result)
+	t1 = time.time()
+	
+	## Find time taken:
+	time_taken = t1-t0
+	
+	## Get an idea of difference
+	shift_val = np.average(np.abs(np.subtract(im_static,im_coregistered)))
+	
+	## return 
+	return im_coregistered, shift_val, time_taken
 
 class MidpointNormalize(matplotlib.colors.Normalize):
-    def __init__(self, vmin, vmax, midpoint=0, clip=False):
-        self.midpoint = midpoint
-        matplotlib.colors.Normalize.__init__(self, vmin, vmax, clip)
+	def __init__(self, vmin, vmax, midpoint=0, clip=False):
+		self.midpoint = midpoint
+		matplotlib.colors.Normalize.__init__(self, vmin, vmax, clip)
 
-    def __call__(self, value, clip=None):
-        normalized_min = max(0, 1 / 2 * (1 - abs((self.midpoint - self.vmin) / (self.midpoint - self.vmax))))
-        normalized_max = min(1, 1 / 2 * (1 + abs((self.vmax - self.midpoint) / (self.midpoint - self.vmin))))
-        normalized_mid = 0.5
-        x, y = [self.vmin, self.midpoint, self.vmax], [normalized_min, normalized_mid, normalized_max]
-        return np.ma.masked_array(np.interp(value, x, y))
-    
-    
+	def __call__(self, value, clip=None):
+		normalized_min = max(0, 1 / 2 * (1 - abs((self.midpoint - self.vmin) / (self.midpoint - self.vmax))))
+		normalized_max = min(1, 1 / 2 * (1 + abs((self.vmax - self.midpoint) / (self.midpoint - self.vmin))))
+		normalized_mid = 0.5
+		x, y = [self.vmin, self.midpoint, self.vmax], [normalized_min, normalized_mid, normalized_max]
+		return np.ma.masked_array(np.interp(value, x, y))
+	
+	
 def PlotCoRegistered(im_static, im_shifted, im_coregistered, **kwargs):
-    """
-    
-    kwargs: 
-        - ShowPlot False(True)
-        - SavePlot False(True)
-        - SavingPathWithName (default '')
-    
-    """
-    try:
-        SavingPathWithName = kwargs['SavingPathWithName']
-    except KeyError:
-        SavingPathWithName = ''
-    
-    try:
-        SavePlot = kwargs['SavePlot']
-    except KeyError:
-        SavePlot = False
-        
-    try:
-        ShowPlot = kwargs['ShowPlot']
-    except KeyError:
-        ShowPlot = False
-        
-    images_diff_0 = np.subtract(im_shifted.astype('float64'), im_static.astype('float64'))
-    images_diff_0_avg = np.average(np.abs(images_diff_0))
+	"""
+	
+	kwargs: 
+		- ShowPlot False(True)
+		- SavePlot False(True)
+		- SavingPathWithName (default '')
+	
+	"""
+	try:
+		SavingPathWithName = kwargs['SavingPathWithName']
+	except KeyError:
+		SavingPathWithName = ''
+	
+	try:
+		SavePlot = kwargs['SavePlot']
+	except KeyError:
+		SavePlot = False
+		
+	try:
+		ShowPlot = kwargs['ShowPlot']
+	except KeyError:
+		ShowPlot = False
+		
+	images_diff_0 = np.subtract(im_shifted.astype('float64'), im_static.astype('float64'))
+	images_diff_0_avg = np.average(np.abs(images_diff_0))
 #     images_diff_0_std = np.std(np.abs(images_diff_0))
-    images_diff_cr = np.subtract(im_coregistered.astype('float64'), im_static.astype('float64'))
-    images_diff_cr_avg = np.average(np.abs(images_diff_cr))
+	images_diff_cr = np.subtract(im_coregistered.astype('float64'), im_static.astype('float64'))
+	images_diff_cr_avg = np.average(np.abs(images_diff_cr))
 #     images_diff_cr_std = np.average(np.std(images_diff_cr))
-    
-    mmm, MMM = 0, 255
-    mm0, MM0 = FindPlottingRange(images_diff_0)
-    mm, MM = FindPlottingRange(images_diff_cr)
-    
-    norm = MidpointNormalize(vmin=mm0, vmax=MM0, midpoint=0)
-    cmap = 'RdBu_r'
-    
-    fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(12,7))
-    im00 = ax[0,0].imshow(im_static, cmap='gray',vmin=mmm, vmax=MMM)
-    ax[0,0].set_title('Static Image')
-    divider = make_axes_locatable(ax[0,0])
-    cax = divider.append_axes('right', size='5%', pad=0.05)
-    cbar = fig.colorbar(im00, cax=cax, orientation='vertical')
+	
+	mmm, MMM = 0, 255
+	mm0, MM0 = FindPlottingRange(images_diff_0)
+	mm, MM = FindPlottingRange(images_diff_cr)
+	
+	norm = MidpointNormalize(vmin=mm0, vmax=MM0, midpoint=0)
+	cmap = 'RdBu_r'
+	
+	fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(12,7))
+	im00 = ax[0,0].imshow(im_static, cmap='gray',vmin=mmm, vmax=MMM)
+	ax[0,0].set_title('Static Image')
+	divider = make_axes_locatable(ax[0,0])
+	cax = divider.append_axes('right', size='5%', pad=0.05)
+	cbar = fig.colorbar(im00, cax=cax, orientation='vertical')
 
-    im01 = ax[0,1].imshow(im_shifted, cmap='gray',vmin=mmm, vmax=MMM)
-    ax[0,1].set_title('Shifted Image')
-    divider = make_axes_locatable(ax[0,1])
-    cax = divider.append_axes('right', size='5%', pad=0.05)
-    cbar = fig.colorbar(im01, cax=cax, orientation='vertical')
+	im01 = ax[0,1].imshow(im_shifted, cmap='gray',vmin=mmm, vmax=MMM)
+	ax[0,1].set_title('Shifted Image')
+	divider = make_axes_locatable(ax[0,1])
+	cax = divider.append_axes('right', size='5%', pad=0.05)
+	cbar = fig.colorbar(im01, cax=cax, orientation='vertical')
 
-    im02 = ax[0,2].imshow(images_diff_0, cmap=cmap, norm=norm)
-    ax[0,2].set_title(f'Difference (no registration)\n avg {images_diff_0_avg:.2f}')
-    divider = make_axes_locatable(ax[0,2])
-    cax = divider.append_axes('right', size='5%', pad=0.05)
-    cbar = fig.colorbar(im02, cax=cax, orientation='vertical')
-    
-    im10 = ax[1,0].imshow(im_static, cmap='gray',vmin=mmm, vmax=MMM)
-    ax[1,0].set_title('Static Image')
-    divider = make_axes_locatable(ax[1,0])
-    cax = divider.append_axes('right', size='5%', pad=0.05)
-    cbar = fig.colorbar(im10, cax=cax, orientation='vertical')
+	im02 = ax[0,2].imshow(images_diff_0, cmap=cmap, norm=norm)
+	ax[0,2].set_title(f'Difference (no registration)\n avg {images_diff_0_avg:.2f}')
+	divider = make_axes_locatable(ax[0,2])
+	cax = divider.append_axes('right', size='5%', pad=0.05)
+	cbar = fig.colorbar(im02, cax=cax, orientation='vertical')
+	
+	im10 = ax[1,0].imshow(im_static, cmap='gray',vmin=mmm, vmax=MMM)
+	ax[1,0].set_title('Static Image')
+	divider = make_axes_locatable(ax[1,0])
+	cax = divider.append_axes('right', size='5%', pad=0.05)
+	cbar = fig.colorbar(im10, cax=cax, orientation='vertical')
 
-    im11 = ax[1,1].imshow(im_coregistered, cmap='gray',vmin=mmm, vmax=MMM)
-    ax[1,1].set_title('Coregistered Image')
-    divider = make_axes_locatable(ax[1,1])
-    cax = divider.append_axes('right', size='5%', pad=0.05)
-    cbar = fig.colorbar(im11, cax=cax, orientation='vertical')
+	im11 = ax[1,1].imshow(im_coregistered, cmap='gray',vmin=mmm, vmax=MMM)
+	ax[1,1].set_title('Coregistered Image')
+	divider = make_axes_locatable(ax[1,1])
+	cax = divider.append_axes('right', size='5%', pad=0.05)
+	cbar = fig.colorbar(im11, cax=cax, orientation='vertical')
 
-    im12 = ax[1,2].imshow(images_diff_cr, cmap=cmap, norm=norm_shift)
-    ax[1,2].set_title(f'Difference (with registration)\n avg {images_diff_cr_avg:.2f}')
-    divider = make_axes_locatable(ax[1,2])
-    cax = divider.append_axes('right', size='5%', pad=0.05)
-    cbar = fig.colorbar(im12, cax=cax, orientation='vertical')
+	im12 = ax[1,2].imshow(images_diff_cr, cmap=cmap, norm=norm_shift)
+	ax[1,2].set_title(f'Difference (with registration)\n avg {images_diff_cr_avg:.2f}')
+	divider = make_axes_locatable(ax[1,2])
+	cax = divider.append_axes('right', size='5%', pad=0.05)
+	cbar = fig.colorbar(im12, cax=cax, orientation='vertical')
 
-    ## Add grid to help see changes in images
-    (YY, XX) = im_static.shape
-    xm, ym = int(XX/2), int(YY/2)
-    xmm, ymm = int(xm/2), int(ym/2)
-    x_points = [xmm, xm, xm+xmm, 3*xmm]
-    y_points = [ymm, ym, ym+ymm, 3*ymm]
-    for i in range(0,3):
-        for j in range(0,2):
-            ax[j,i].set_xticks([])
-            ax[j,i].set_yticks([])
-            for k in range(0,4):
-                ax[j,i].axvline(x_points[k], c='limegreen', ls='dotted')
-                ax[j,i].axhline(y_points[k], c='limegreen', ls='dotted')
+	## Add grid to help see changes in images
+	(YY, XX) = im_static.shape
+	xm, ym = int(XX/2), int(YY/2)
+	xmm, ymm = int(xm/2), int(ym/2)
+	x_points = [xmm, xm, xm+xmm, 3*xmm]
+	y_points = [ymm, ym, ym+ymm, 3*ymm]
+	for i in range(0,3):
+		for j in range(0,2):
+			ax[j,i].set_xticks([])
+			ax[j,i].set_yticks([])
+			for k in range(0,4):
+				ax[j,i].axvline(x_points[k], c='limegreen', ls='dotted')
+				ax[j,i].axhline(y_points[k], c='limegreen', ls='dotted')
 
-    plt.tight_layout()
-    if SavePlot:
-        if '.png' not in SavingPathWithName:
-            SavingPathWithName = SavingPathWithName+'_CoRegistration.png'
-        print(f'Saving figure @ {SavingPathWithName}')
-        print(f'   Set SavingPathWithName=\'path\' to set saving path')
-        plt.savefig(f'{SavingPathWithName}')
-    if ShowPlot:
-        plt.show()
-    else:
-        plt.close()
+	plt.tight_layout()
+	if SavePlot:
+		if '.png' not in SavingPathWithName:
+			SavingPathWithName = SavingPathWithName+'_CoRegistration.png'
+		print(f'Saving figure @ {SavingPathWithName}')
+		print(f'   Set SavingPathWithName=\'path\' to set saving path')
+		plt.savefig(f'{SavingPathWithName}')
+	if ShowPlot:
+		plt.show()
+	else:
+		plt.close()
 
 
 
@@ -1519,8 +1532,8 @@ def MakeHypercubeVideo(Hypercube, SavingPathWithName, **kwargs):
 
 	out = cv2.VideoWriter(SavingPathWithName, cv2.VideoWriter_fourcc(*'mp4v'), fps, (XX, YY), False)
 	for i in range(NN):
-	    data = Hypercube[i,:,:].astype('uint8')
-	    out.write(data)
+		data = Hypercube[i,:,:].astype('uint8')
+		out.write(data)
 	out.release()
 
 
@@ -1560,25 +1573,31 @@ def PlotHypercube(Hypercube, **kwargs):
 		ShowPlot = True
 
 
+	NN, YY, XX = Hypercube.shape
+
 	nn = 0
 	plt.close()
 	fig, ax = plt.subplots(nrows=4, ncols=4, figsize=(8,8))
 	for j in range(0,4):
-	    for i in range(0,4):
-	        if nn<NN:
-	        	if Wavelengths==0:
-	        		RGB = (0,0,0) ## Set title to black if no wavelength input
-	        	else:
-		            wav = Wavelengths[nn]
-		            RGB = wavelength_to_rgb(wav)
-	            ax[j,i].imshow(Hypercube[nn,:,:], cmap='gray', vmin=0, vmax=np.amax(Hypercube))
-	            ax[j,i].set_title(f'{wav} nm', c=RGB)
-	            ax[j,i].set_xticks([])
-	            ax[j,i].set_yticks([])
-	            nn = nn+1
-	        else:
-	            ax[j,i].set_xticks([])
-	            ax[j,i].set_yticks([])
+		for i in range(0,4):
+			if nn<NN:
+				if Wavelengths==0:
+					wav = 0
+					RGB = (0,0,0) ## Set title to black if no wavelength input
+				else:
+					wav = Wavelengths[nn]
+					RGB = wavelength_to_rgb(wav)
+				ax[j,i].imshow(Hypercube[nn,:,:], cmap='gray', vmin=0, vmax=np.amax(Hypercube))
+				if wav==0:
+					ax[j,i].set_title(f'{nn} wavelength', c=RGB)
+				else:
+					ax[j,i].set_title(f'{wav} nm', c=RGB)
+				ax[j,i].set_xticks([])
+				ax[j,i].set_yticks([])
+				nn = nn+1
+			else:
+				ax[j,i].set_xticks([])
+				ax[j,i].set_yticks([])
 	if SavePlot:
 		if '.png' not in SavingPathWithName:
 			SavingPathWithName = SavingPathWithName+'_Hypercube.png'
@@ -1603,4 +1622,4 @@ def PlotHypercube(Hypercube, **kwargs):
 
 
 
-    
+	
