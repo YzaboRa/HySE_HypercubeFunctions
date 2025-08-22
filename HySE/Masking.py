067381223a76964a0c0c3c418689c022afbe10f5
Isabelle Racicot <racicot.isabelle@gmail.com>
2025-08-22 18:48:11 +0100

    Masking

--- HySE/Masking.py
+++ HySE/Masking.py
@@ -20,15 +20,107 @@ import inspect
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 matplotlib.rcParams.update({'font.size': 14})
 plt.rcParams["font.family"] = "arial"
-
+from scipy.ndimage import median_filter
 
 # import HySE_ImportData
 import HySE.UserTools
 # import HySE.ManipulateHypercube
 
 
+
+def RemoveSpecularReflections_Frame(frame, **kwargs):
+	"""
+	Detects and removes specular reflections from an image frame.
+	
+	Inputs:
+		- frame : Input 2D image (Y, X), float values (dark-subtracted, 0–~255).
+		- kwargs:
+			- Help
+			- k = 3 : Threshold factor (default). Reflection mask is frame > mean + k*std.
+				Lower k = more masking
+			- Cutoff : When specified, use this value as threshold for specular reflections
+			- NeighborhoodSize = 5 : When specified (default), used to compute median value around
+				masked area and use a fill value
+			- FillValue  : When specified, replace masked pixels by this value
+	
+	Outputs:
+	
+		- frame_corrected : Image with specular reflections replaced by local median values/fill value.
+		- mask :  Binary mask of specular reflections (uint8, 0 or 1).
+	"""
+	Help = kwargs.get('Help', False)
+	if Help:
+		print(inspect.getdoc(ApplyTransform))
+		return 0, 0
+	FillValue = kwargs.get('FillValue')
+	Cutoff = kwargs.get('Cutoff')
+	
+	# Ensure float array
+	img = frame.astype(np.float32)
+	
+	# Step 1: Identify reflections
+	mean_val = np.mean(img)
+	std_val = np.std(img)
+	if Cutoff is None:
+		k = kwargs.get('k', 3)
+		threshold = mean_val + k * std_val
+	else:
+		threshold = Cutoff
+	mask = (img > threshold).astype(np.uint8)
+
+	frame_corrected = img.copy()
+	if FillValue is not None:
+		frame_corrected[mask == 1] = FillValue
+		
+	else:
+		NeighborhoodSize = kwargs.get('NeighborhoodSize', 5)
+		median_img = median_filter(img, size=NeighborhoodSize)
+		frame_corrected[mask == 1] = median_img[mask == 1]
+
+
+	return frame_corrected, mask
+
+
+def RemoveSpecularReflections(Frames, **kwargs):
+	"""
+	Detects and removes specular reflections from a set of frames (Nwav, Y, X).
+	
+	Inputs:
+		- frames : Input frames (Nwav, Y, X), float values (dark-subtracted, 0–~255).
+		- kwargs:
+			- Help
+			- k = 3 : Threshold factor (default). Reflection mask is frame > mean + k*std.
+				Lower k = more masking
+			- Cutoff : When specified, use this value as threshold for specular reflections
+			- NeighborhoodSize = 5 : When specified (default), used to compute median value around
+				masked area and use a fill value
+			- FillValue  : When specified, replace masked pixels by this value
+	
+	Outputs:
+	
+		- MaskedFrames : Images with specular reflections replaced by local median values/fill 
+			value for each frame.
+		- AllMasks :  Binary masks of specular reflections (uint8, 0 or 1) for each frame.
+	"""
+	(Nwav, Y, X) = Frames.shape
+	MaskedFrames = []
+	AllMasks = []
+	for n in range(0,Nwav):
+		frame = Frames[n,:,:]
+		frame_corrected, mask = RemoveSpecularReflections_Frame(frame, **kwargs)
+		MaskedFrames.append(frame_corrected)
+		AllMasks.append(mask)
+	AllMasks = np.array(AllMasks)
+	MaskedFrames = np.array(MaskedFrames)
+	return MaskedFrames, AllMasks
+
+
+
+
+
 def GetStandardMask(WhiteCalibration, **kwargs):
 	"""
+	OLD
 	Returns a array (mask) that includes every pixel with a value below a threshold.
 	Threshold can be indicated, or set automatically to 1.
 	Best to use white calibration data for optimal mask
@@ -55,6 +147,7 @@ def GetStandardMask(WhiteCalibration, **kwargs):
 
 def ConvertMaskToBinary(mask):
 	"""
+	OLD
 	Invert mask (1-mask_value) and convert to binary format
 
 	Inputs: 
@@ -69,6 +162,7 @@ def ConvertMaskToBinary(mask):
 
 def BooleanMaskOperation(bool_white, bool_wav):
 	"""
+	OLD
 	Old function to handle boolean operation for masks
 
 	"""
@@ -83,15 +177,30 @@ def BooleanMaskOperation(bool_white, bool_wav):
 # 	result = vectorized_function(mask_white, mask_shifted)
 # 	return result
 
-# def CombineMasks(mask_white, mask_shifted):
-# 	mask = np.ma.mask_or(mask_white, mask_shifted)
-# 	mask = mask*1
-# 	mask = mask.astype('uint8')
-# 	return mask
+def CombineMasks(mask1, mask2):
+	"""
+	OLD
+	Function that combines two maks (OR operator).
+	A pixel that is masked in either (or both) of the masks will be masked in the final mask
+
+	Inputs:
+		- mask1
+		- mask2
+
+	Outputs:
+		- combined_mask
+
+
+	"""
+	combined_mask = np.ma.mask_or(mask1, mask2)
+	combined_mask = combined_mask*1
+	combined_mask = mask.astype('uint8')
+	return combined_mask
 	
 
 def GetMask(frame, **kwargs):
 	'''
+	OLD
 	Inputs:
 		- frame (2D array)
 		- kwargs
